4c248ec30918613f00d119b15a868b46f1c069bb
Soenke Domroese <soenke@soenke-domroese.de>
2012-04-11 22:35:18 +0200

    Added Holo Theme for Android 3.0.x and 4.0.x

--- gen/de/ardunoid/weightlogger/R.java
+++ gen/de/ardunoid/weightlogger/R.java
@@ -15,21 +15,21 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btnAdd=0x7f060009;
-        public static final int edtBodyBone=0x7f060008;
-        public static final int edtBodyWater=0x7f060006;
-        public static final int edtBodyfat=0x7f060004;
-        public static final int edtWeight=0x7f060002;
-        public static final int imageView1=0x7f060000;
-        public static final int mySimpleXYPlot=0x7f06000a;
-        public static final int optExit=0x7f06000e;
-        public static final int optShowAll=0x7f06000d;
-        public static final int optShowGraph=0x7f06000c;
-        public static final int text1=0x7f06000b;
-        public static final int txtBodyBone=0x7f060007;
-        public static final int txtBodyFat=0x7f060003;
-        public static final int txtBodyWater=0x7f060005;
-        public static final int txtWeight=0x7f060001;
+        public static final int btnAdd=0x7f070009;
+        public static final int edtBodyBone=0x7f070008;
+        public static final int edtBodyWater=0x7f070006;
+        public static final int edtBodyfat=0x7f070004;
+        public static final int edtWeight=0x7f070002;
+        public static final int imageView1=0x7f070000;
+        public static final int mySimpleXYPlot=0x7f07000a;
+        public static final int optExit=0x7f07000e;
+        public static final int optShowAll=0x7f07000d;
+        public static final int optShowGraph=0x7f07000c;
+        public static final int text1=0x7f07000b;
+        public static final int txtBodyBone=0x7f070007;
+        public static final int txtBodyFat=0x7f070003;
+        public static final int txtBodyWater=0x7f070005;
+        public static final int txtWeight=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -37,7 +37,7 @@ public final class R {
         public static final int weightshow=0x7f030002;
     }
     public static final class menu {
-        public static final int menu=0x7f050000;
+        public static final int menu=0x7f060000;
     }
     public static final class string {
         public static final int BodyBone=0x7f040006;
@@ -50,5 +50,8 @@ public final class R {
         public static final int hello=0x7f040000;
         public static final int showAll=0x7f040008;
         public static final int weight=0x7f040003;
+    }
+    public static final class style {
+        public static final int Theme_MyHolo=0x7f050000;
     }
 }
